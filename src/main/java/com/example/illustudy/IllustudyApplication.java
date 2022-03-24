2d24e6e402ad899288bc35abce1cb1398ce7b22d
kishi <kuro.1955.illust@gmail.com>
2022-03-24 16:24:57 +0900

    first commit

---
+++ src/main/java/com/example/illustudy/IllustudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.illustudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllustudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllustudyApplication.class, args);
+	}
+
+}
